52721f88531ea66d5bdeeacbe11e528486654197
Chance Yan <chanceyan@Chances-MacBook-Pro.local>
2025-02-13 19:57:30 -0500

    Branch Info Added!

--- Hello.R
+++ Hello.R
@@ -3,3 +3,5 @@
 #After that's done you can commit any files in the top right window of R (make sure to add a comment)
 #after that enter the code given by Github to allow access to the repository. You'll need an access token (make sure to allow all permissions)
 #When that's done your code should be uploaded. After doing the initial steps you can just hit commit, add comment, then push to update code.
+
+#You can either group files by folder or by pushing to branches. 
