39a9717c446e16ff2c3d55c742205e8cced497ca
Chance Yan <chanceyan@Chances-MacBook-Pro.local>
2025-02-13 19:45:27 -0500

    Updated directions on how to push with R

--- Hello.R
+++ Hello.R
@@ -1,2 +1,5 @@
-#Hello world!
-#what happens if I try to update this file?
+#HOW TO PUSH R STUDIO FILE TO GITHUB
+#Create a R project with R files in it. You want to create the R project as a version control and input your github repository link
+#After that's done you can commit any files in the top right window of R (make sure to add a comment)
+#after that enter the code given by Github to allow access to the repository. You'll need an access token (make sure to allow all permissions)
+#When that's done your code should be uploaded. After doing the initial steps you can just hit commit, add comment, then push to update code.
